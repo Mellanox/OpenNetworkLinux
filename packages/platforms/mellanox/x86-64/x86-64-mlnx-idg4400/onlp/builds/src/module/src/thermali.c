74e4bc98274133f38bcb742867b873aa05476078
rinat zelig <rinat@mellanox.com>
2017-07-16 10:56:59 +0300

    handle system led efefcted by thermal and psu led effected by psu status
    Change-Id: I0a9723795f40bd8d52650d531410026c216241e9
    Signed-off-by: rinat zelig <rinat@mellanox.com>

--- packages/platforms/mellanox/x86-64/x86-64-mlnx-idg4400/onlp/builds/src/module/src/thermali.c
+++ packages/platforms/mellanox/x86-64/x86-64-mlnx-idg4400/onlp/builds/src/module/src/thermali.c
@@ -69,24 +69,6 @@ nps_thermal_threshold_t;
         }                                       \
     } while(0)
 
-enum onlp_thermal_id
-{
-    THERMAL_RESERVED = 0,
-    THERMAL_CPU_CORE_0,
-    THERMAL_CPU_CORE_1,
-	THERMAL_CPU_CORE_2,
-	THERMAL_CPU_CORE_3,
-    THERMAL_CPU_PACK,
-	THERMAL_FRONT,
-	THERMAL_REAR,
-	THERMAL_PEX,
-	THERMAL_NPS,
-	THERMAL_TCAM,
-	THERMAL_MNB,
-    THERMAL_ON_PSU1,
-    THERMAL_ON_PSU2,
-};
-
 static char* last_path[] =  /* must map with onlp_thermal_id */
 {
     "reserved",
